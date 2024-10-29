c4c10c8ca8ce8d0c1b3b57f849df16edeb86dfbd
iprimavera <elsprimavera55@gmail.com>
2024-10-29 17:57:50 +0100

    added what is for

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('mi-boton').addEventListener('click', function() {
-    const parrafo = document.getElementById('mi-parrafo');
-    parrafo.style.color = parrafo.style.color === 'red' ? 'grey' : 'red';
-});
+// document.getElementById('mi-boton').addEventListener('click', function() {
+//    const parrafo = document.getElementById('mi-parrafo');
+//    parrafo.style.color = parrafo.style.color === 'red' ? 'grey' : 'red';
+//});
