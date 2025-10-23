b21fb101753ccf1fdec0406cebeff8c633cceb73
Ivan <148491638+iprimavera@users.noreply.github.com>
2025-10-23 11:35:29 +0200

    Clean

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-// Función para cargar el footer desde el archivo footer.html
+
 async function cargarFooter() {
     const response = await fetch('../includes/footer.html');
     document.getElementById('footer').innerHTML = await response.text();
 }
 
-// Cargar el footer cuando la página esté lista
 window.onload = () => {
     cargarFooter();
 }
